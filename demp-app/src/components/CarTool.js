9a31256229b546b11c52990eb6074606e4ffbae2
Rokas Balbieris <rokas.balbieris@yahoo.com>
2019-10-06 12:24:33 +0300

    added form to add an object to the table + comments

--- demp-app/src/components/CarTool.js
+++ demp-app/src/components/CarTool.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const CarTool = (props)=> {
+    //***destructuring props : export const Cartool = ({ cars: initialCars }) ... (with alliasing)
 
-   
+   const [ 
+       carForm, // state data
+        setCarForm // function to update the  state data and re-render
+    ] = useState({
+       make: '',
+       model: '',
+       year: 1900,
+       color: '',
+       price: 0,
+   }); // initial state data, and is only used on the initial render
+
+   const [ cars,setCars ] = useState(props.cars.concat());
+   //***const [ cars,setCars ] = useState(initialCars.concat());
+
+   const change = (e) => {
+     setCarForm({
+         ...carForm,
+        [ e.target.name ]: e.target.type === 'number' ? Number(e.target.value):e.target.value,
+     });
+   };
+
+        //*destructuring 
+        //    const change = ({target}) => {
+        //     setCarForm({
+        //         ...carForm,
+        //        [ target.name ]: target.type === 'number' ? Number(target.value):target.value,
+        //     });
+        //   };
+
+        //**destructuring and using alliasing to destructure even more
+        //    const change = ({target: { name, type, value } }) => {
+        //     setCarForm({
+        //         ...carForm,
+        //        [ name ]: type === 'number' ? Number(value) : value,
+        //     });
+        //   };
+
+   const addCar = () => {
+
+        setCars(cars.concat({
+            ...carForm,
+            id: Math.max(...cars.map(c => c.id)) + 1,
+        }));
+
+        setCarForm({
+            make: '',
+            model: '',
+            year: 1900,
+            color: '',
+            price: 0,
+        });
+
+   };
 
     return <>
         <header>
@@ -36,7 +89,7 @@ export const CarTool = (props)=> {
                     <td>Red</td>
                     <td>120000</td>
                 </tr>    */}
-                {props.cars.map(car => <tr key={car.id}>
+                {cars.map(car => <tr key={car.id}>
                     <td>{car.id}</td>
                     <td>{car.make}</td>
                     <td>{car.model}</td>
@@ -49,6 +102,34 @@ export const CarTool = (props)=> {
                 <tr>Footer of table</tr>
             </tfoot> */}
         </table>
+        <form>
+            <div>
+                <label htmlFor="make-input">Make:</label>
+                <input type="text" id="make-input" name="make"
+                value={carForm.make} onChange={change} />
+            </div>
+            <div>
+                <label htmlFor="model-input">Model:</label>
+                <input type="text" id="model-input" name="model"
+                value={carForm.model}  onChange={change} />
+            </div>
+            <div>
+                <label htmlFor="year-input">Year:</label>
+                <input type="number" id="year-input" name="year"
+                value={carForm.year}  onChange={change} />
+            </div>
+            <div>
+                <label htmlFor="color-input">Color:</label>
+                <input type="text" id="color-input" name="color"
+                value={carForm.color}  onChange={change} />
+            </div>
+            <div>
+                <label htmlFor="price-input">Price:</label>
+                <input type="number" id="price-input" name="price"
+                value={carForm.price}  onChange={change} />
+            </div>
+            <button type="button" onClick={addCar}>Add Car</button>
+        </form>
 
     </>;
 }
